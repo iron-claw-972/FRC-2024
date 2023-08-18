608871dba73bc5ffd0591baafc41b3a5cbbf411f
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-08-18 08:20:08 -0700

    Remove reference to deleted commands

--- src/main/java/frc/robot/util/FeedForwardCharacterizationData.java
+++ src/main/java/frc/robot/util/FeedForwardCharacterizationData.java
@@ -7,8 +7,6 @@ import lib.PolynomialRegression;
 
 /**
  * A class for storing and processing feedforward characterization data. Used in automatic feedforward characterization.
- * @see frc.robot.commands.test.DriveFeedForwardCharacterization
- * @see frc.robot.commands.test.SteerFeedForwardCharacterizationSingle
  */
 public class FeedForwardCharacterizationData {
   private PolynomialRegression m_regression;
