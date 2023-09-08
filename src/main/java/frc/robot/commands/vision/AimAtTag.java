7189a155e2ec27baced831d375d10e5401059b63
Kyle-Eldridge <eldk6798@lgsstudent.org>
2023-09-08 16:29:25 -0700

    Finish ChaseAprilTag (PID needs tuning)

--- src/main/java/frc/robot/commands/vision/AimAtTag.java
+++ src/main/java/frc/robot/commands/vision/AimAtTag.java
@@ -1,35 +1,54 @@
 package frc.robot.commands.vision;
 
+import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.constants.miscConstants.FieldConstants;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.util.Vision;
 
 public class AimAtTag extends CommandBase {
-  Vision m_vision; 
-  Drivetrain m_swerve; 
-  PIDController m_controller; 
-  public AimAtTag(Vision vision, Drivetrain swerve){
-    m_vision = vision;
+  Drivetrain m_swerve;
+  PIDController m_pid;
+
+  public AimAtTag(Drivetrain swerve){
     m_swerve = swerve; 
-    m_controller = new PIDController(0, 0, 0); 
+    m_pid = new PIDController(0, 0, 0); 
+    m_pid.setTolerance(Units.degreesToRadians(1));
+    addRequirements(swerve);
+  }
+
+  @Override
+  public void initialize(){
+    double dist = Double.POSITIVE_INFINITY;
+    Translation2d closest = new Translation2d();
+    Translation2d driveTranslation = m_swerve.getPose().getTranslation();
+    for(AprilTag tag : FieldConstants.kAprilTags){
+      Translation2d translation = tag.pose.toPose2d().getTranslation();
+      double dist2 = driveTranslation.getDistance(translation);
+      if(dist2 < dist){
+        dist = dist2;
+        closest = translation;
+      }
+    }
+    m_pid.setSetpoint(Math.atan2(closest.getY() - driveTranslation.getY(), closest.getX() - driveTranslation.getX()));
   }
   
   @Override
-  public void execute() { 
-    m_con
+  public void execute() {
+    double speed = m_pid.calculate(m_swerve.getPose().getRotation().getRadians());
+    m_swerve.drive(0, 0, speed, true, false);
   }
 
   @Override
   public void end(boolean interrupted) {
-
+    m_swerve.stop();
   }
 
   @Override
   public boolean isFinished() {
-    return false; 
+    return m_pid.atSetpoint();
   }
-
-
 }
 
