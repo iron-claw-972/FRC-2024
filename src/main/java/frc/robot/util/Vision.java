897bb7af78a825518fa2dc584278a88ae4c2c454
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-10-04 10:32:36 -0700

    Switch to PhotonVision (still has errors)

--- src/main/java/frc/robot/util/Vision.java
+++ src/main/java/frc/robot/util/Vision.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.commons.lang3.Functions;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
@@ -18,30 +17,19 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.vision.AimAtTag;
 import frc.robot.commands.vision.CalculateStdDevs;
 import frc.robot.commands.vision.ChaseTag;
 import frc.robot.commands.vision.ReturnData;
-import frc.robot.commands.vision.TakeSnapshots;
 import frc.robot.commands.vision.TestVisionDistance;
-import frc.robot.constants.Constants;
 import frc.robot.constants.miscConstants.FieldConstants;
 import frc.robot.constants.miscConstants.VisionConstants;
 import frc.robot.subsystems.Drivetrain;
@@ -50,10 +38,6 @@ import frc.robot.subsystems.Drivetrain;
 public class Vision {
 
   private ShuffleboardTab m_shuffleboardTab;
-
-  private Debouncer m_debouncer;
-
-  private ArrayList<double[]> m_yaws;
 
   // The field layout
   private AprilTagFieldLayout m_aprilTagFieldLayout;
@@ -65,8 +49,6 @@ public class Vision {
    */
   public Vision(ShuffleboardTab shuffleboardTab, ArrayList<Pair<String, Transform3d>> camList) {
     m_shuffleboardTab = shuffleboardTab;
-
-    m_debouncer = new Debouncer(0.05, DebounceType.kRising);
     
     try {
       // Try to find the field layout
@@ -83,147 +65,6 @@ public class Vision {
     for (int i = 0; i < camList.size(); i++) {
       m_cameras.add(new VisionCamera(camList.get(i).getFirst(), camList.get(i).getSecond()));
     }
-    
-    m_yaws = new ArrayList<double[]>();
-    m_yaws.add(new double[]{0, 0});
-  }
-
-  /**
-   * Get the horizontal offset in degrees from the crosshair to the target
-   * @return offset in degrees
-   */
-  public double getHorizontalOffsetDegrees(){
-    return m_tx.getDouble(0);
-  }
-
-  /**
-   * Get the vertical offset in degrees from the crosshair to the target
-   * @return offset in degrees
-   */
-  public double getVerticalOffsetDegrees(){
-    return m_ty.getDouble(0);
-  }
-
-  /**
-   * Get the target area (percentage of the image (screen) that the target takes up)
-   * @return percentage
-   */
-  public double getTargetAreaPercentage(){
-    return m_ta.getDouble(0);
-  }
-
-  /**
-   * Returns the total latency in ms from the limelight
-   * @return the latency as a double
-   */
-  public double getLatency(){
-    return m_tl.getDouble(0)+m_cl.getDouble(0);
-  }
-
-  /**
-   * Returns whether or not a valid target was detected after being passed through a debouncer 
-   * to make sure that we are really locked on the target. We wait 0.05 seconds and check whether or not we are still locked on our target. 
-   * @return true or false 
-   */
-  public boolean validTargetDetected(){
-    return m_debouncer.calculate(m_tv.getDouble(0)==1); 
-  }
-
-  /**
-   * Returns the robot pose as a double array
-   * @return A double array with x, y, z, roll, pitch, yaw
-   */
-  public double[] getRobotPose(){
-    double[] pose = m_robotPoseVision.getDoubleArray(new double[6]);
-    if(Constants.kLogging){
-      LogManager.addDoubleArray("Vision/pose", pose);
-    }
-    return pose;
-  }
-
-  /**
-   * Returns the robot pose as a double array calculated using the target height and camera position
-   * @param yaw The yaw of the robot to use in the calculation (radians)
-   * @return A double array with x, y, z, roll, pitch, yaw
-   */
-  public double[] getRobotPose(double yaw){
-    int id = (int) m_tid.getDouble(0)-1;
-    if(id<=0||id>FieldConstants.kAprilTags.size()){
-      return new double[6];
-    }
-    Pose3d target = FieldConstants.kAprilTags.get(id).pose;
-    double[] camera = m_cameraPose.getDoubleArray(new double[6]);
-    double fieldRelativeAngle = yaw+Units.degreesToRadians(camera[5]-getHorizontalOffsetDegrees());
-    double verticalAngle = Units.degreesToRadians(camera[4]+getVerticalOffsetDegrees());
-    double dist = (target.getZ()-camera[2])/Math.tan(verticalAngle);
-    double x = target.getX()-Math.cos(fieldRelativeAngle)*dist;
-    double y = target.getY()-Math.sin(fieldRelativeAngle)*dist;
-    x -= camera[0]*Math.cos(yaw) + camera[1]*Math.sin(yaw);
-    y -= camera[0]*Math.sin(yaw) + camera[1]*Math.cos(yaw);
-    double[] pose = new double[]{x, y, 0, 0, 0, yaw};
-    if(Constants.kLogging){
-      LogManager.addDouble("Vision/tag dist", dist);
-      LogManager.addDoubleArray("Vision/pose", pose);
-    }
-    return pose;
-  }
-
-  /**
-   * Returns the estimated position as a Pose2d
-   * @return a Pose2d
-   */
-  public Pose2d getPose2d(){
-    if(validTargetDetected()){
-      double[] pose = getRobotPose();
-      if(pose[0] < 0 || pose[1] < 0){
-        return null;
-      }
-      return new Pose2d(pose[0], pose[1], Rotation2d.fromDegrees(pose[5]));
-    }else{
-      return null;
-    }
-  }
-
-  /**
-   * Returns the estimated position as a Pose2d calculated using the target height and camera position
-   * @param useManualCalculation If it should use manual calculations (recommended true)
-   * @return a Pose2d
-   */
-  public Pose2d getPose2d(boolean useManualCalculation){
-    if(!useManualCalculation){
-      return getPose2d();
-    }
-    if(validTargetDetected()){
-      updateYaws();
-      double[] pose = getRobotPose(m_yaws.get(0)[0]);
-      if(pose[0] < 0 || pose[1] < 0){
-        return null;
-      }
-      return new Pose2d(pose[0], pose[1], Rotation2d.fromDegrees(pose[5]));
-    }else{
-      return null;
-    }
-  }
-
-  /**
-   * Returns the Pose2d and the time stamp in seconds
-   * @return a pair with a Pose2d and double
-   */
-  public Pair<Pose2d, Double> getPose2dWithTimeStamp(){
-    return new Pair<Pose2d, Double>(getPose2d(), Timer.getFPGATimestamp()-getLatency()/1000);
-  }
-
-  /**
-   * Returns the Pose2d and the time stamp in seconds calculated using the target height and camera position
-   * @param useManualCalculation If it should use manual calculations (recommended true)
-   * @return a pair with a Pose2d and double
-   */
-  public Pair<Pose2d, Double> getPose2dWithTimeStamp(boolean useManualCalculation){
-    return new Pair<Pose2d, Double>(getPose2d(useManualCalculation), getTimeStamp());
-  }
-
-  public double getTimeStamp(){
-    return Timer.getFPGATimestamp()-getLatency()/1000;
   }
 
   /**
@@ -244,19 +85,8 @@ public class Vision {
     m_shuffleboardTab.add("Distance test (backward)", new TestVisionDistance(-0.1, drive, this));
   }
 
-  public void addYaw(double yaw){
-    m_yaws.add(new double[]{yaw, Timer.getFPGATimestamp()});
-    updateYaws();
-  }
-
-  public void updateYaws(){
-    if(m_yaws.size() > 1 && m_yaws.get(1)[1] < getTimeStamp()){
-      m_yaws.remove(0);
-    }
-  }
-
   /**
-   * Gets the pose as a Pose2d
+   * Gets the pose as a Pose2d using PhotonVision
    * @param referencePoses The reference poses in order of preference, null poses will be skipped
    * @return The pose of the robot, or null if it can't see april tags
    */
@@ -280,7 +110,7 @@ public class Vision {
           .plus(estimatedPoses.get(1).estimatedPose.toPose2d().getTranslation())
           .div(2),
         
-        new Rotation2d(Functions.modulusMidpoint(
+        new Rotation2d(MathUtils.modulusMidpoint(
           estimatedPoses.get(0).estimatedPose.toPose2d().getRotation().getRadians(),
           estimatedPoses.get(1).estimatedPose.toPose2d().getRotation().getRadians(),
           -Math.PI, Math.PI)
