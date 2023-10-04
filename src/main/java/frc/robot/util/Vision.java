ace071ff66ac77c7799e85ee29d4be4bbc9f1b6e
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-10-04 08:25:59 -0700

    Start combining 2023 and 2024 code

--- src/main/java/frc/robot/util/Vision.java
+++ src/main/java/frc/robot/util/Vision.java
@@ -1,86 +1,91 @@
 package frc.robot.util;
 
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.vision.ReturnData;
-import frc.robot.commands.vision.TakeSnapshots;
-import frc.robot.commands.vision.TestVisionDistance;
-import frc.robot.commands.vision.AimAtTag;
-import frc.robot.commands.vision.CalculateStdDevs;
-import frc.robot.commands.vision.ChaseTag;
-import frc.robot.constants.Constants;
-import frc.robot.constants.miscConstants.FieldConstants;
-import frc.robot.subsystems.Drivetrain;
-
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import org.apache.commons.lang3.Functions;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.common.hardware.VisionLEDMode;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.vision.AimAtTag;
+import frc.robot.commands.vision.CalculateStdDevs;
+import frc.robot.commands.vision.ChaseTag;
+import frc.robot.commands.vision.ReturnData;
+import frc.robot.commands.vision.TakeSnapshots;
+import frc.robot.commands.vision.TestVisionDistance;
+import frc.robot.constants.Constants;
+import frc.robot.constants.miscConstants.FieldConstants;
+import frc.robot.constants.miscConstants.VisionConstants;
+import frc.robot.subsystems.Drivetrain;
 
 // Vision and it's commands are adapted from Iron Claw's FRC2022, FRC2023, and: https://www.youtube.com/watch?v=TG9KAa2EGzQ&t=1439s
 public class Vision {
 
   private ShuffleboardTab m_shuffleboardTab;
 
-  private NetworkTable m_visionTable; 
-
-  private Debouncer m_tvDebouncer;
-
-  private NetworkTableEntry m_tv;
-  private NetworkTableEntry m_tx;
-  private NetworkTableEntry m_ty;
-  private NetworkTableEntry m_ta;
-  private NetworkTableEntry m_tl;
-  private NetworkTableEntry m_cl;
-  private NetworkTableEntry m_robotPoseVision;
-  private NetworkTableEntry m_snapshot;
-  private NetworkTableEntry m_tid;
-  private NetworkTableEntry m_cameraPose;
+  private Debouncer m_debouncer;
 
   private ArrayList<double[]> m_yaws;
+
+  // The field layout
+  private AprilTagFieldLayout m_aprilTagFieldLayout;
+  // A list of the cameras on the robot
+  private ArrayList<VisionCamera> m_cameras = new ArrayList<>();
 
   /**
    * Creates a new instance of Vision and sets up the limelight NetworkTable and the SmartDashboard
    */
-  public Vision(ShuffleboardTab shuffleboardTab) {
+  public Vision(ShuffleboardTab shuffleboardTab, ArrayList<Pair<String, Transform3d>> camList) {
     m_shuffleboardTab = shuffleboardTab;
 
-    //get the limelight table from the default NetworkTable instance
-    m_visionTable = NetworkTableInstance.getDefault().getTable("limelight");
+    m_debouncer = new Debouncer(0.05, DebounceType.kRising);
+    
+    try {
+      // Try to find the field layout
+      m_aprilTagFieldLayout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
+    } catch (IOException e) {
+      // If it can't find it, use the layout in the constants
+      m_aprilTagFieldLayout = new AprilTagFieldLayout(FieldConstants.kAprilTags, FieldConstants.kFieldLength, FieldConstants.kFieldWidth);
+      DriverStation.reportWarning("Could not find k2023ChargedUp.m_resourceFile, check that GradleRIO is updated to at least 2023.2.1 in build.gradle",  e.getStackTrace());
+    }
+    // Sets the origin to the right side of the blue alliance wall
+    m_aprilTagFieldLayout.setOrigin(OriginPosition.kBlueAllianceWallRightSide);
 
-    m_tvDebouncer = new Debouncer(0.05, DebounceType.kRising);
-
-    //from the table, get various entries that contain data 
-    m_tv = m_visionTable.getEntry("tv"); 
-    m_tx = m_visionTable.getEntry("tx"); 
-    m_ty = m_visionTable.getEntry("ty"); 
-    m_ta = m_visionTable.getEntry("ta"); 
-    m_tl = m_visionTable.getEntry("tl"); 
-    m_cl = m_visionTable.getEntry("cl"); 
-    m_robotPoseVision = m_visionTable.getEntry("botpose_wpiblue");
-    m_snapshot = m_visionTable.getEntry("snapshot");
-    m_tid = m_visionTable.getEntry("tid");
-    m_cameraPose = m_visionTable.getEntry("camerapose_robotspace");
-
+    // Puts the cameras in an array list
+    for (int i = 0; i < camList.size(); i++) {
+      m_cameras.add(new VisionCamera(camList.get(i).getFirst(), camList.get(i).getSecond()));
+    }
+    
     m_yaws = new ArrayList<double[]>();
     m_yaws.add(new double[]{0, 0});
-  }
-
-  /**
-   * Pipeline 0 detects an april tag with an ID of 0, pipeline 1 detects an april tag with an ID of 1
-   * @param pipeline
-   */
-  public void switchPipelines(int pipeline){
-    m_visionTable.getEntry("pipeline").setNumber(pipeline); 
   }
 
   /**
@@ -121,7 +126,7 @@ public class Vision {
    * @return true or false 
    */
   public boolean validTargetDetected(){
-    return m_tvDebouncer.calculate(m_tv.getDouble(0)==1); 
+    return m_debouncer.calculate(m_tv.getDouble(0)==1); 
   }
 
   /**
@@ -237,8 +242,6 @@ public class Vision {
     m_shuffleboardTab.add("Distance test (forward)", new TestVisionDistance(0.1, drive, this));
     SmartDashboard.putData("Vision distance test (backward)", new TestVisionDistance(-0.1, drive, this));
     m_shuffleboardTab.add("Distance test (backward)", new TestVisionDistance(-0.1, drive, this));
-    SmartDashboard.putData("Take vision snapshots", new TakeSnapshots(m_snapshot));
-    m_shuffleboardTab.add("Take snapshots", new TakeSnapshots(m_snapshot));
   }
 
   public void addYaw(double yaw){
@@ -249,6 +252,141 @@ public class Vision {
   public void updateYaws(){
     if(m_yaws.size() > 1 && m_yaws.get(1)[1] < getTimeStamp()){
       m_yaws.remove(0);
+    }
+  }
+
+  /**
+   * Gets the pose as a Pose2d
+   * @param referencePoses The reference poses in order of preference, null poses will be skipped
+   * @return The pose of the robot, or null if it can't see april tags
+   */
+  public Pose2d getPose2d(Pose2d... referencePoses){
+    Pose2d referencePose = new Pose2d();
+    for (Pose2d checkReferencePose:referencePoses){
+      if (checkReferencePose != null) {
+        referencePose = checkReferencePose;
+        break;
+      }
+    }
+    ArrayList<EstimatedRobotPose> estimatedPoses = getEstimatedPoses(referencePose);
+    Translation2d translation = new Translation2d();
+    double rotation = 0;
+    
+    if (estimatedPoses.size() == 1) return estimatedPoses.get(0).estimatedPose.toPose2d();
+    
+    if (estimatedPoses.size() == 2) {
+      return new Pose2d(
+        estimatedPoses.get(0).estimatedPose.toPose2d().getTranslation()
+          .plus(estimatedPoses.get(1).estimatedPose.toPose2d().getTranslation())
+          .div(2),
+        
+        new Rotation2d(Functions.modulusMidpoint(
+          estimatedPoses.get(0).estimatedPose.toPose2d().getRotation().getRadians(),
+          estimatedPoses.get(1).estimatedPose.toPose2d().getRotation().getRadians(),
+          -Math.PI, Math.PI)
+        )
+      );
+    }
+          
+    //TODO: VERY LOW PRIORITY FOR FUTURE ROBOTS, make the rotation average work with more than 2 cameras
+    // for(int i = 0; i < estimatedPoses.size(); i ++){
+    //   translation=translation.plus(estimatedPoses.get(i).estimatedPose.toPose2d().getTranslation());
+    // }
+
+    // if(posesUsed>0){
+    //   return new Pose2d(translation.div(estimatedPoses.size()), new Rotation2d());
+    // }
+    return null;
+  }
+
+  public AprilTagFieldLayout getAprilTagFieldLayout(){
+    return m_aprilTagFieldLayout;
+  }
+
+  /**
+   * Gets the pose of an april tag
+   * @param id AprilTag id (1-8)
+   * @return Pose3d of the AprilTag
+   */
+  public Pose3d getTagPose(int id){
+    if(id < 1 || id > 8){
+      System.out.println("Tried to find the pose of april tag "+id);
+      return null;
+    }
+    return getAprilTagFieldLayout().getTagPose(id).get();
+  }
+
+  /**
+   * Returns where it thinks the robot is
+   * @param referencePose The pose to use as a reference, usually the previous robot pose
+   * @return An array list of estimated poses, one for each camera that can see an april tag
+   */
+  public ArrayList<EstimatedRobotPose> getEstimatedPoses(Pose2d referencePose) {
+    ArrayList<EstimatedRobotPose> estimatedPoses = new ArrayList<>();
+    for (int i = 0; i < m_cameras.size(); i++) {
+      Optional<EstimatedRobotPose> estimatedPose = m_cameras.get(i).getEstimatedPose(referencePose);
+      // If the camera can see an april tag that exists, add it to the array list
+      // April tags that don't exist might return a result that is present but doesn't have a pose
+      if (estimatedPose.isPresent() && estimatedPose.get().estimatedPose != null) {
+        estimatedPoses.add(estimatedPose.get());
+        LogManager.addDoubleArray("Vision/camera " + i + "/estimated pose2d", new double[] {
+          estimatedPose.get().estimatedPose.getX(),
+          estimatedPose.get().estimatedPose.getY(),
+          estimatedPose.get().estimatedPose.getRotation().getZ()
+        });
+      }
+    }
+    return estimatedPoses;
+  }
+  
+  private class VisionCamera {
+    PhotonCamera camera;
+    PhotonPoseEstimator photonPoseEstimator;
+  
+    /**
+     * Stores information about a camera
+     * @param cameraName The name of the camera on PhotonVision
+     * @param robotToCam The transformation from the robot to the camera
+     */
+    public VisionCamera(String cameraName, Transform3d robotToCam) {
+      camera = new PhotonCamera(cameraName);
+      photonPoseEstimator = new PhotonPoseEstimator(
+        m_aprilTagFieldLayout, 
+        PoseStrategy.MULTI_TAG_PNP, 
+        camera, 
+        robotToCam
+      );
+      photonPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.CLOSEST_TO_REFERENCE_POSE);
+      photonPoseEstimator.setReferencePose(new Pose2d());
+      camera.setLED(VisionLEDMode.kOn);
+    }
+  
+    /**
+     * Gets the estimated pose from the camera
+     * @param referencePose Pose to use for reference, usually the previous estimated robot pose
+     * @return estimated robot pose
+     */
+    public Optional<EstimatedRobotPose> getEstimatedPose(Pose2d referencePose) {
+      photonPoseEstimator.setReferencePose(referencePose);
+
+      PhotonPipelineResult cameraResult = camera.getLatestResult();
+      
+      // if there is a target detected and not in the past, 
+      // check the ambiguity isn't too high
+      if (cameraResult.hasTargets() && cameraResult.getTimestampSeconds() > 0) {
+        // go through all the targets
+        List<PhotonTrackedTarget> targetsUsed = cameraResult.targets;
+        for (int i = 0; i < targetsUsed.size(); i++) {
+          // check their ambiguity, if it is above the highest wanted amount, return nothing
+          if (targetsUsed.get(i).getPoseAmbiguity() > VisionConstants.highestAmbiguity) {
+            return Optional.empty();
+          }
+        }
+      }
+
+      Optional<EstimatedRobotPose> pose = photonPoseEstimator.update(cameraResult);
+
+      return pose;
     }
   }
 }
