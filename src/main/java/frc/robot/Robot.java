22bd931d5beb7497b694d177bfeda890a2bd46e8
Tyrus <businessomga@gmail.com>
2023-10-17 15:20:13 -0700

    Remove set vision enabled call

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -108,9 +108,6 @@ public class Robot extends TimedRobot {
     public void teleopInit() {
 //        robotContainer.resetModules();
 
-        // In teleop, may enable vision for use for grid/shelf alignment
-//        robotContainer.setVisionEnabled(true);
-
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
