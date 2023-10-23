748df16399b031a321fa6eb82dc84a5e7730fb24
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-10-23 10:01:05 -0700

    Change camera name

--- src/main/java/frc/robot/constants/miscConstants/VisionConstants.java
+++ src/main/java/frc/robot/constants/miscConstants/VisionConstants.java
@@ -36,7 +36,7 @@ public class VisionConstants {
 
   // The limelight pose
   public static final ArrayList<Pair<String, Transform3d>> kCameras = new ArrayList<Pair<String, Transform3d>>(List.of(
-    new Pair<String, Transform3d>("limelight",
+    new Pair<String, Transform3d>("Camera1",
     new Transform3d(new Translation3d(0.5, 0, 0.1), new Rotation3d(0, 0, 0)))
   ));
 }
