6b5581ac93ad87b6cdd791ecd0ee329919d86732
Tyrus <businessomga@gmail.com>
2023-10-27 15:18:25 -0700

    Add comment to drivetrain class

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,6 +2,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * This class should include methods that will be overridden by {@link DrivetrainImpl} to be used on an actual robot
+ * with a swerve style drivetrain. This class will be used when the current robot does not have a swerve style
+ * drivetrain, as defined in {@link frc.robot.RobotId}.
+ */
 @SubsystemImpl(DrivetrainImpl.class)
 public class Drivetrain extends SubsystemBase {
 
