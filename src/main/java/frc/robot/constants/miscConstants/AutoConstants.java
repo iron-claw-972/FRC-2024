8783993e8b67233617c053eb5ad5aa36c98feb58
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2023-12-01 07:50:31 -0800

    Add todo

--- src/main/java/frc/robot/constants/miscConstants/AutoConstants.java
+++ src/main/java/frc/robot/constants/miscConstants/AutoConstants.java
@@ -8,6 +8,9 @@ public class AutoConstants {
   // Pathplanner output folder should be src/main/deploy/pathplanner
   public static final String kTrajectoryDirectory = "pathplanner/";
 
+  //TODO: Replace these values after testing
+  // public static final double kMaxAutoSpeed = 4.3; // m/s
+  // public static final double kMaxAutoAccel = 2.1; // m/s^2
   public static final double kMaxAutoSpeed = 0.5; // m/s
   public static final double kMaxAutoAccel = 1.5; // m/s^2
 }
