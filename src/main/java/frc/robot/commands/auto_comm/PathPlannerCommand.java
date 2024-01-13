d0a719e440a2eeb68be6effc9764e253b2f3b738
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2024-01-13 15:54:01 -0800

    Fix all errors

--- src/main/java/frc/robot/commands/auto_comm/PathPlannerCommand.java
+++ src/main/java/frc/robot/commands/auto_comm/PathPlannerCommand.java
@@ -6,20 +6,22 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.commands.FollowPathHolonomic;
+import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.path.PathPlannerTrajectory;
 import com.pathplanner.lib.path.PathPoint;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.ReplanningConfig;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.constants.AutoConstants;
+import frc.robot.constants.swerve.DriveConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.ConversionUtils;
 import frc.robot.util.PathGroupLoader;
@@ -32,18 +34,22 @@ public class PathPlannerCommand extends SequentialCommandGroup {
   }
 
   public PathPlannerCommand(ArrayList<PathPoint> waypoints, Drivetrain drive, boolean useAllianceColor) {
-    this(new ArrayList<PathPlannerTrajectory>(Arrays.asList(PathPlanner.generatePath(
-      new PathConstraints(AutoConstants.MAX_AUTO_SPEED, AutoConstants.MAX_AUTO_ACCEL),
-      waypoints.get(0),
-      waypoints.get(1)
+    this(new ArrayList<PathPlannerPath>(Arrays.asList(new PathPlannerPath(
+      List.of(
+        waypoints.get(0).position,
+        waypoints.get(1).position),
+      new PathConstraints(AutoConstants.MAX_AUTO_SPEED, AutoConstants.MAX_AUTO_ACCEL, DriveConstants.kMaxAngularSpeed, DriveConstants.kMaxAngularAccel),
+      new GoalEndState(0, waypoints.get(1).rotationTarget.getTarget())
     ))), 0, drive, false, useAllianceColor, true);
   }
 
   public PathPlannerCommand(ArrayList<PathPoint> waypoints, Drivetrain drive, boolean useAllianceColor, double maxSpeed, double maxAccel) {
-    this(new ArrayList<PathPlannerTrajectory>(Arrays.asList(PathPlanner.generatePath(
-      new PathConstraints(maxSpeed, maxAccel),
-      waypoints.get(0),
-      waypoints.get(1)
+    this(new ArrayList<PathPlannerPath>(Arrays.asList(new PathPlannerPath(
+      List.of(
+        waypoints.get(0).position,
+        waypoints.get(1).position),
+      new PathConstraints(maxSpeed, maxAccel, DriveConstants.kMaxAngularSpeed, DriveConstants.kMaxAngularAccel),
+      new GoalEndState(0, waypoints.get(1).rotationTarget.getTarget())
     ))), 0, drive, false, useAllianceColor, true);
   }
   
@@ -55,11 +61,11 @@ public class PathPlannerCommand extends SequentialCommandGroup {
     this(PathGroupLoader.getPathGroup(pathGroupName), pathIndex, drive, resetPose, true, false); 
   }
 
-  public PathPlannerCommand(List<PathPlannerTrajectory> pathGroup, int pathIndex, Drivetrain drive, boolean resetPose){
+  public PathPlannerCommand(List<PathPlannerPath> pathGroup, int pathIndex, Drivetrain drive, boolean resetPose){
     this(pathGroup, pathIndex, drive, resetPose, true, false);
   }
 
-  public PathPlannerCommand(List<PathPlannerTrajectory> pathGroup, int pathIndex, Drivetrain drive, boolean resetPose, boolean useAllianceColor, boolean isPerpetual) {
+  public PathPlannerCommand(List<PathPlannerPath> pathGroup, int pathIndex, Drivetrain drive, boolean resetPose, boolean useAllianceColor, boolean isPerpetual) {
 
     addRequirements(drive);
     if (pathIndex < 0 || pathIndex > pathGroup.size() - 1) {
@@ -68,10 +74,16 @@ public class PathPlannerCommand extends SequentialCommandGroup {
     
     addCommands(
       new InstantCommand( () -> {
-        PathPlannerTrajectory path = PathPlannerTrajectory.transformTrajectoryForAlliance(
-          pathGroup.get(pathIndex), DriverStation.getAlliance());
+        PathPlannerPath path = pathGroup.get(pathIndex);
+        if(DriverStation.getAlliance().get()==Alliance.Red){
+          path.flipPath();
+        }
         if (resetPose) {
-          drive.resetOdometry(ConversionUtils.absolutePoseToPathPlannerPose(path.getInitialHolonomicPose(), DriverStation.getAlliance()));
+          drive.resetOdometry(path.getPreviewStartingHolonomicPose());
+        }
+        //TODO: This might be unnecessary, maybe delete it
+        if(DriverStation.getAlliance().get()==Alliance.Red){
+          path.flipPath();
         }
       }),
       createSwerveControllerCommand(
@@ -79,10 +91,10 @@ public class PathPlannerCommand extends SequentialCommandGroup {
         useAllianceColor ? // Pose supplier
           () -> ConversionUtils.absolutePoseToPathPlannerPose(drive.getPose(), DriverStation.getAlliance().get()) : 
           () -> drive.getPose(), 
-        drive.getXController(), // X controller can't normal PID as pathplanner has Feed Forward 
-        drive.getYController(), // Y controller can't normal PID as pathplanner has Feed Forward 
-        drive.getRotationController(), // Rotation controller can't normal PID as pathplanner has Feed Forward 
+        ()-> drive.getChassisSpeeds(),
         (chassisSpeeds) -> { drive.setChassisSpeeds(chassisSpeeds, false); }, // chassis Speeds consumer
+        AutoConstants.MAX_AUTO_SPEED,
+        Math.sqrt(2)*DriveConstants.kTrackWidth/2,
         useAllianceColor,  // use Alliance color
         drive, // Requires this drive subsystem
         isPerpetual
@@ -91,31 +103,32 @@ public class PathPlannerCommand extends SequentialCommandGroup {
   }
   
   public static FollowPathHolonomic createSwerveControllerCommand(
-      PathPlannerTrajectory trajectory, Supplier<Pose2d> poseSupplier, 
-      PIDController xController, PIDController yController, PIDController rotationController, 
-      Consumer<ChassisSpeeds> outputChassisSpeeds, boolean useAllianceColor, Drivetrain drive, boolean isPerpetual) {
+      PathPlannerPath path, Supplier<Pose2d> poseSupplier, 
+      Supplier<ChassisSpeeds> speedSupplier,
+      Consumer<ChassisSpeeds> outputChassisSpeeds, 
+      double maxModuleSpeed,
+      double driveBaseReadius,
+      boolean useAllianceColor, Drivetrain drive, boolean isPerpetual) {
     if (isPerpetual) {
       return new PPSwerveControllerCommandPerpetual(
-        trajectory,
+        path,
         poseSupplier,
-        xController,
-        yController,
-        rotationController,
+        speedSupplier,
         outputChassisSpeeds,
+        new HolonomicPathFollowerConfig(maxModuleSpeed, driveBaseReadius, new ReplanningConfig(false, false)),
         useAllianceColor,
         drive
       );
     }
 
     return new FollowPathHolonomic(
-      trajectory,
-      poseSupplier,
-      xController,
-      yController,
-      rotationController,
-      outputChassisSpeeds,
-      useAllianceColor,
-      drive
+        path,
+        poseSupplier,
+        speedSupplier,
+        outputChassisSpeeds,
+        new HolonomicPathFollowerConfig(maxModuleSpeed, driveBaseReadius, new ReplanningConfig(false, false)),
+        ()-> useAllianceColor,
+        drive
     );
   }
 }
