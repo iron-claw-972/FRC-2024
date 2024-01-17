f9e983bc48ced8ca360064b7daa45f3024dc6c4c
Tyrus <businessomga@gmail.com>
2024-01-17 15:23:29 -0800

    intake

---
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -0,0 +1,20 @@
+package frc.robot.constants;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+public final class IntakeConstants {
+
+    // motor port
+    public static final int MOTOR_ID = 9;
+
+    // intake speeds
+    public static final double INTAKE_POWER = 0.8;
+    public static final NeutralMode NEUTRAL_MODE = NeutralMode.Brake;
+
+    // Current limits
+    public static final int CONTINUOUS_CURRENT_LIMIT = 25;
+    public static final int PEAK_CURRENT_LIMIT = 55;
+    public static final double PEAK_CURRENT_DURATION = 0.1;
+    public static final boolean ENABLE_CURRENT_LIMIT = true;
+
+}
--- src/main/java/frc/robot/subsystems/gpm_subsystem/Intake.java
+++ src/main/java/frc/robot/subsystems/gpm_subsystem/Intake.java
@@ -1,5 +1,62 @@
 package frc.robot.subsystems.gpm_subsystem;
 
-public class Intake {
-    
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.IntakeConstants;
+import frc.robot.constants.globalConst;
+import frc.robot.util.MotorFactory;
+
+
+public class Intake extends SubsystemBase {
+
+    public enum IntakeMode {
+        INTAKE(IntakeConstants.INTAKE_POWER), DISABLED(0)
+        ;
+
+        private double power;
+
+        IntakeMode(double power) {
+            this.power = power;
+        }
+
+        public double getPower() {
+            return power;
+        }
+    }
+
+    private final WPI_TalonFX motor;
+
+    private boolean hasNote;
+    private IntakeMode mode;
+
+
+    public Intake() {
+        motor = MotorFactory.createTalonFX(IntakeConstants.MOTOR_ID, globalConst.RIO_CAN);
+        motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(
+                IntakeConstants.ENABLE_CURRENT_LIMIT,
+                IntakeConstants.CONTINUOUS_CURRENT_LIMIT,
+                IntakeConstants.PEAK_CURRENT_LIMIT,
+                IntakeConstants.PEAK_CURRENT_DURATION
+        ));
+        motor.setNeutralMode(IntakeConstants.NEUTRAL_MODE);
+        motor.enableVoltageCompensation(true);
+
+        hasNote = false;
+        mode = IntakeMode.DISABLED;
+    }
+
+    public void setMode(IntakeMode mode) {
+        this.mode = mode;
+    }
+
+    @Override
+    public void periodic() {
+        motor.set(mode.getPower());
+    }
+
+    public boolean hasNote() {
+        return hasNote;
+    }
+
 }
