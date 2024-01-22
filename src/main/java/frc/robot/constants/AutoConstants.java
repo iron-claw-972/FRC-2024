52bc0375fb33ad2ea42f1dfaf09dca505d7b5ab5
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2024-01-22 12:16:33 -0800

    Change replanningConfig and PID

--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -17,10 +17,10 @@ public class AutoConstants {
     public static final double MAX_AUTO_SPEED = 4.3; // m/s
     public static final double MAX_AUTO_ACCEL = 2.1; // m/s^2
 
-    public static final PIDConstants translationConstants = new PIDConstants(0,0,0);
+    public static final PIDConstants translationConstants = new PIDConstants(5,0,0);
     public static final PIDConstants rotationConstants = new PIDConstants(0,0,0);
 
-    public static final ReplanningConfig replanningConfig = new ReplanningConfig(false, false);
+    public static final ReplanningConfig replanningConfig = new ReplanningConfig(true, false);
 
     public static final HolonomicPathFollowerConfig config = new HolonomicPathFollowerConfig(
         translationConstants,
