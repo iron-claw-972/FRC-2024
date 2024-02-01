2ce547ddcde8b51ed61d8227f1bd22876700fb21
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2024-02-01 11:50:38 -0800

    remove subsystems from RobotID (unused)

--- src/main/java/frc/robot/RobotId.java
+++ src/main/java/frc/robot/RobotId.java
@@ -1,12 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.module.Module;
-
-
-import java.util.List;
-
 /**
  * Set of known Robot Names.
  * <p>The name of a robot in the RoboRIO's persistent memory.
@@ -15,22 +8,8 @@ import java.util.List;
  */
 public enum RobotId {
     Default,
-    SwerveCompetition(Drivetrain.class, Module.class), SwerveTest(Drivetrain.class),
+    SwerveCompetition, SwerveTest,
     ClassBot1, ClassBot2, ClassBot3, ClassBot4;
-
-    /**
-     * List of subsystems to create when the robot is instantiated.
-     */
-    private final List<Class<? extends SubsystemBase>> subsystems;
-
-    @SafeVarargs
-    RobotId(Class<? extends SubsystemBase>... subsystems) {
-        this.subsystems = List.of(subsystems);
-    }
-
-    public List<Class<? extends SubsystemBase>> getSubsystems() {
-        return subsystems;
-    }
 
     public boolean isClassBot() {
         return this == ClassBot1 || this == ClassBot2 || this == ClassBot3 || this == ClassBot4;
@@ -39,6 +18,4 @@ public enum RobotId {
     public boolean isSwerveBot() {
         return this == SwerveCompetition || this == SwerveTest;
     }
-    
-
 }
