cc8d076150071cf44a80f3861f87035324e4c970
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2024-02-07 17:01:39 -0800

    Change camera location

--- src/main/java/frc/robot/constants/miscConstants/VisionConstants.java
+++ src/main/java/frc/robot/constants/miscConstants/VisionConstants.java
@@ -98,8 +98,8 @@ public class VisionConstants {
     new Pair<String, Transform3d>(
       "Camera1",
       new Transform3d(
-        new Translation3d(Units.inchesToMeters(8.996), Units.inchesToMeters(6.48), Units.inchesToMeters(37.44)),
-        new Rotation3d(0, Units.degreesToRadians(18), 0)
+        new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(26)),
+        new Rotation3d(0, Units.degreesToRadians(0), 0)
       )),
     new Pair<String, Transform3d>(
       "Camera2",
