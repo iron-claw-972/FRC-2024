2ee863e3afb9aedfed73875862b50ba736da1117
Tyrus <businessomga@gmail.com>
2024-02-09 16:41:23 -0800

    remove forever param for intakenote command

--- src/main/java/frc/robot/commands/gpm/IntakeNote.java
+++ src/main/java/frc/robot/commands/gpm/IntakeNote.java
@@ -8,14 +8,12 @@ import frc.robot.subsystems.gpm_subsystem.Intake;
 public class IntakeNote extends Command {
 
     private final Intake intake;
-    private final boolean forever;
     private final Debouncer stallDebouncer;
 
 
     public IntakeNote(Intake intake, boolean forever) {
         addRequirements(intake);
         this.intake = intake;
-        this.forever = forever;
         this.stallDebouncer = new Debouncer(IntakeConstants.INTAKE_STALL_TIME, Debouncer.DebounceType.kBoth);
     }
 
@@ -27,9 +25,6 @@ public class IntakeNote extends Command {
     // TODO: This code might need some adjusting, as the cone passes through the intake.
     @Override
     public boolean isFinished() {
-        if (forever) {
-            return false;
-        }
         return stallDebouncer.calculate(
                 intake.getCurrent() >= IntakeConstants.INTAKE_CURRENT_STOP
         );
