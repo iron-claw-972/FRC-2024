212e4340217eb2f214fa995043396570a5bc23c6
Ash1R <ashir.rao26@gmail.com>
2024-02-10 14:51:40 -0800

    very basic indexer code

---
+++ src/main/java/frc/robot/constants/StorageIndexConstants.java
@@ -0,0 +1,14 @@
+package frc.robot.constants;
+
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+public class StorageIndexConstants {
+    public static final int indexMotorID = 0;
+    public static final int indexBeamBreak = 0; // no clue what we're using, but this is what windup's doing
+    public static final NeutralModeValue indexNeutralMode = NeutralModeValue.Brake;
+
+    public static final int stallLimit = 25;
+
+    public static final double intakeSpeed = 0;
+
+}
--- src/main/java/frc/robot/subsystems/gpm_subsystem/StorageIndex.java
+++ src/main/java/frc/robot/subsystems/gpm_subsystem/StorageIndex.java
@@ -1,5 +1,64 @@
 package frc.robot.subsystems.gpm_subsystem;
 
-public class StorageIndex {
-    
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.util.MotorFactory;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.StorageIndexConstants;
+import frc.robot.constants.Constants;
+
+public class StorageIndex extends SubsystemBase {
+
+    private final CANSparkMax m_indexmotor;
+    // private final SparkAbsoluteEncoder encoder;
+
+    DigitalInput m_indexBeamBreak;
+
+    public StorageIndex() {
+        m_indexmotor = MotorFactory.createSparkMAX(StorageIndexConstants.indexMotorID, MotorType.kBrushless,
+                StorageIndexConstants.stallLimit);
+        // encoder = m_indexmotor.getAbsoluteEncoder(Type.kDutyCycle);
+
+        m_indexBeamBreak = new DigitalInput(StorageIndexConstants.indexBeamBreak);
+
+        m_indexmotor.setInverted(false);
+
+        // they did something to do with CAN Frames here
+
+    }
+
+    public void runIndex(double speed) {
+        m_indexmotor.set(speed);
+    }
+
+    public void runIndex() {
+        m_indexmotor.set(StorageIndexConstants.intakeSpeed);
+    }
+
+    public void stopIndex() {
+        m_indexmotor.set(0);
+    }
+
+    public void ejectBack(double speed) {
+        if (hasNote()) {
+            this.runIndex((-1.0) * speed);
+        }
+    }
+
+    public void ejectFront(double speed) {
+        if (hasNote()) {
+            this.runIndex(speed);
+        }
+    }
+
+    public boolean hasNote() {
+        return m_indexBeamBreak.get() ? false : true;
+    }
+
 }
