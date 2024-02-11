3646252b1a6d5e4a1f247f79ea8cc395128aed68
Kyle-Eldridge <eldk6798@lgsstudent.org>
2024-02-11 13:01:15 -0800

    Revert "Revert "Merge branch 'outtake_draft' of https://github.com/iron-claw-972/FRC-2024 into outtake_draft""
    This reverts commit c57c70128c0ccbece5291373dd17b91384ca0ae1.

--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -17,7 +17,8 @@ public class AutoConstants {
     public static final double MAX_AUTO_SPEED = 4.3; // m/s
     public static final double MAX_AUTO_ACCEL = 2.1; // m/s^2
 
-    public static final PIDConstants translationConstants = new PIDConstants(0,0,0);
+    // TODO: Tune these
+    public static final PIDConstants translationConstants = new PIDConstants(1,0,0);
     public static final PIDConstants rotationConstants = new PIDConstants(0,0,0);
 
     public static final ReplanningConfig replanningConfig = new ReplanningConfig(true, false);
