5d39cb359a4836dca5ec04aed9f064aacec37d13
Arnie495 <arnieincyberland@gmail.com>
2024-02-11 11:41:36 -0800

    Made some style changes.
    Moved PID stuff from ShooterConstants to Shooter
    Moved conve conversion functions from ConversionUtils to Shooter
    Split method that return arrays of two items into two methods.
    Added methods to get the difference between the two motor speeds to Shooter.

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -6,21 +6,4 @@ public class ShooterConstants {
     public static final int BOTTOM_MOTOR_ID = 99;
     public static final int TOP_MOTOR_ID = 99;
 
-    // constants for Pids
-    public static final double TOP_P = 0.00005;
-    public static final double TOP_I = 0;
-    public static final double TOP_D = 0;
-
-    public static final double S = 0;
-    public static final double V = 1.0/6000;
-
-    public static final double BOTTOM_P = 0.00005;
-    public static final double BOTTOM_I = 0;
-    public static final double BOTTOM_D = 0;
-
-    /**
-     * In RPM
-     */
-    public static final double TOLERANCE = 10;
-
 }
--- src/main/java/frc/robot/subsystems/gpm/Shooter.java
+++ src/main/java/frc/robot/subsystems/gpm/Shooter.java
@@ -8,23 +8,38 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ShooterConstants;
-import frc.robot.util.ConversionUtils;
 
 public class Shooter extends SubsystemBase {
+    // constants for Pids
+    public static final double TOP_P = 0.00005;
+    public static final double TOP_I = 0;
+    public static final double TOP_D = 0;
+
+    public static final double S = 0;
+    public static final double V = 1.0/6000;
+
+    public static final double BOTTOM_P = 0.00005;
+    public static final double BOTTOM_I = 0;
+    public static final double BOTTOM_D = 0;
+
+    /**
+     * In RPM
+     */
+    public static final double TOLERANCE = 10;
+
     private final CANSparkFlex bottomMotor = new CANSparkFlex(ShooterConstants.BOTTOM_MOTOR_ID, MotorType.kBrushless);
     private final CANSparkFlex topMotor = new CANSparkFlex(ShooterConstants.TOP_MOTOR_ID, MotorType.kBrushless);
     private final RelativeEncoder bottomMotorEncoder = bottomMotor.getEncoder();
     private final RelativeEncoder topMotorEncoder = topMotor.getEncoder();
-    private final PIDController topPID = new PIDController(ShooterConstants.TOP_P, ShooterConstants.TOP_I,
-            ShooterConstants.TOP_D);
-    private final PIDController bottomPID = new PIDController(ShooterConstants.BOTTOM_P, ShooterConstants.BOTTOM_I, ShooterConstants.BOTTOM_D);
+    private final PIDController topPID = new PIDController(TOP_P, TOP_I, TOP_D);
+    private final PIDController bottomPID = new PIDController(BOTTOM_P, BOTTOM_I, BOTTOM_D);
 
     // TODO: TUNE THIS
-    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(ShooterConstants.S, ShooterConstants.V);
+    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(S, V);
 
     public Shooter() {
-        topPID.setTolerance(ShooterConstants.TOLERANCE);
-        bottomPID.setTolerance(ShooterConstants.TOLERANCE);
+        topPID.setTolerance(TOLERANCE);
+        bottomPID.setTolerance(TOLERANCE);
         bottomMotor.setInverted(true);
     }
 
@@ -36,11 +51,33 @@ public class Shooter extends SubsystemBase {
         topMotor.set(topPID.calculate(topSpeed) + feedforward.calculate(topPID.getSetpoint()));
         bottomMotor.set(bottomPID.calculate(bottomSpeed) + feedforward.calculate(bottomPID.getSetpoint()));
 
-        SmartDashboard.putNumber("top speed", ConversionUtils.shooterRPMToSpeed(topSpeed));
-        SmartDashboard.putNumber("bottom speed", ConversionUtils.shooterRPMToSpeed(bottomSpeed));
+        SmartDashboard.putNumber("top speed", shooterRPMToSpeed(topSpeed));
+        SmartDashboard.putNumber("bottom speed", shooterRPMToSpeed(bottomSpeed));
         SmartDashboard.putBoolean("at setpoint?", atSetpoint());
-        System.out.println(ConversionUtils.shooterRPMToSpeed(topSpeed) + "," + ConversionUtils.shooterRPMToSpeed(bottomSpeed));
+        System.out.println(shooterRPMToSpeed(topSpeed) + "," + shooterRPMToSpeed(bottomSpeed));
     }
+
+	/**
+	* Converts an RPM to a speed at the surface of the shooter wheels.
+	*
+	* @param  rpm the RPM to convert
+	* @return     the converted value in m/s
+	* @see        shooterSpeedToRPM
+	*/
+	public static double shooterRPMToSpeed(double rpm) {
+			return (rpm / 60) * (4 * Math.PI * 0.0254);
+	}
+
+	/**
+	* Converts a speed at the surface of the shooter wheels to an RPM value.
+	*
+	* @param  speed the speed to convert in m/s
+	* @return       the converted value in RPM
+	* @see          shooterRPMToSpeed
+	*/
+	public static double shooterSpeedToRPM(double speed) {
+			return (speed * 60) / (4 * Math.PI * 0.0254);
+	}
 
 	/**
 	* Sets the speed both shooter motors try to spin up to independantly.
@@ -73,7 +110,7 @@ public class Shooter extends SubsystemBase {
 	*/
     public void setTargetVelocity(double speed) {
         // convert speed to RPM
-        setTargetRPM(ConversionUtils.shooterSpeedToRPM(speed));
+        setTargetRPM(shooterSpeedToRPM(speed));
     }
 
 	/**
@@ -86,25 +123,77 @@ public class Shooter extends SubsystemBase {
     }
 
 	/**
-	* Gets the RPMs of both motors.
+	* Gets the RPM of the top motor via the encoder's getVelocity method.
 	*
-	* @return a list containing [top motor RPM, bottom motor RPM]
-	* @see    getMotorVelocities
+	* @return the top motor's RPM
+	* @see    getBottomMotorRPM
+	* @see    getTopMotorSpeed
 	* @see    atSetpoint
 	*/
-	public double[] getMotorRPMs() {
-		return new double[] {topMotorEncoder.getVelocity(), bottomMotorEncoder.getVelocity()};
+	public double getTopMotorRPM() {
+		return topMotorEncoder.getVelocity();
 	}
 
 	/**
-	* Gets the velocities of both motors.
+	* Gets the RPM of the bottom motor via the encoder's getVelocity method.
 	*
-	* @return an array containing [top motor velocity, bottom motor velocity]
-	* @see    getMotorRPMs
+	* @return the bottom motor's RPM
+	* @see    getTopMotorRPM
+	* @see    getBottomMotorSpeed
 	* @see    atSetpoint
 	*/
-	public double[] getMotorVelocities() {
-		double[] rpms = getMotorRPMs();
-		return new double[] {ConversionUtils.shooterRPMToSpeed(rpms[0]), ConversionUtils.shooterRPMToSpeed(rpms[1])};
+	public double getBottomMotorRPM() {
+		return bottomMotorEncoder.getVelocity();
+	}
+
+	/**
+	* Gets the speed at the surface of the top wheels.
+	*
+	* @return the top wheel's speed in m/s
+	* @see    getBottomMotorSpeed
+	* @see    getTopMotorRPM
+	* @see    atSetpoint
+	*/
+	public double getTopMotorSpeed() {
+		return shooterRPMToSpeed(getTopMotorRPM());
+	}
+
+	/**
+	* Gets the speed at the surface of the bottom wheels.
+	*
+	* @return the bottom wheel's speed in m/s
+	* @see    getTopMotorSpeed
+	* @see    getBottomMotorRPM
+	* @see    atSetpoint
+	*/
+	public double getBottomMotorSpeed() {
+		return shooterRPMToSpeed(getBottomMotorRPM());
+	}
+	
+	/**
+	* Gets the difference in RPM between the top motor and bottom motor.
+	* 
+	* @return the top motor's RPM - the bottom motor-s RPM
+	* @see    getMotorSpeedDifference
+	* @see    getTopMotorRPM
+	* @see    getBottomMotorRPM
+	* @see    atSetpoint
+	*/
+	public double getMotorRPMDifference() {
+		return getTopMotorRPM() - getBottomMotorRPM();
+	}
+	
+	/**
+	* Gets the difference in speed at the circumference of the motor's wheels
+	* between the top motor and bottom motor.
+	* 
+	* @return the top motor's speed - the bottom motor's speed
+	* @see    getMotorRPMDifference
+	* @see    getTopMotorSpeed
+	* @see    getBottomMotorSpeed
+	* @see    atSetpoint
+	*/
+	public double getMotorSpeedDifference() {
+		return getTopMotorSpeed() - getBottomMotorSpeed();
 	}
 }
