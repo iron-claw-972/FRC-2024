d3143675b7f748e61fdd985d8679020ac03c0805
Ash1R <ashir.rao26@gmail.com>
2024-02-18 14:47:08 -0800

    :)

--- src/main/java/frc/robot/commands/ArmToPos.java
+++ src/main/java/frc/robot/commands/ArmToPos.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.gpm.Arm;
+import edu.wpi.first.wpilibj2.command.Command;
 
 //for use with IntakeNote, OuttakeAmp, and Shoot commands
 public class ArmToPos extends Command {
