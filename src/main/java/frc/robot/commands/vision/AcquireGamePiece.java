b4e8829f5eb69f6bd7b05e4bb1e063807829fac1
Ash1R <ashir.rao26@gmail.com>
2024-02-19 14:49:59 -0800

    complete AcquireGamePiece sequence

--- src/main/java/frc/robot/commands/vision/AcquireGamePiece.java
+++ src/main/java/frc/robot/commands/vision/AcquireGamePiece.java
@@ -2,18 +2,31 @@ package frc.robot.commands.vision;
 
 import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.ArmToPos;
+import frc.robot.commands.gpm.IntakeNote;
+
+import frc.robot.constants.ArmConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.DetectedObject;
+import frc.robot.subsystems.gpm.Arm;
+import frc.robot.subsystems.gpm.StorageIndex;
 
-public class AcquireGamePiece extends SequentialCommandGroup{
+public class AcquireGamePiece extends SequentialCommandGroup {
     /**
      * Intakes a game piece
+     * 
      * @param gamePiece The supplier for the game piece to intake
-     * @param drive The drivetrain
+     * @param drive     The drivetrain
      */
-    public AcquireGamePiece(Supplier<DetectedObject> gamePiece, Drivetrain drive){
-        //TODO: Add code to move intake after deciding on a design in 2024
-        addCommands(new DriveToNote(gamePiece, drive));
+    public AcquireGamePiece(Supplier<DetectedObject> gamePiece, Drivetrain drive, Arm arm, Intake intake,
+            StorageIndex index) {
+
+        addCommands(
+                new ParallelCommandGroup(
+                        new DriveToNote(gamePiece, drive),
+                        new ArmToPos(arm, ArmConstants.intakeSetpoint)),
+                new IntakeNote(intake, index));
     }
 }
