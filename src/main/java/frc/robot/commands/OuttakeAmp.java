4d9e138e31990f280a867af7a3080d7f5738064a
Ash1R <ashir.rao26@gmail.com>
2024-02-21 11:31:14 -0800

    :)

--- src/main/java/frc/robot/commands/OuttakeAmp.java
+++ src/main/java/frc/robot/commands/OuttakeAmp.java
@@ -24,49 +24,49 @@ import frc.robot.subsystems.gpm.StorageIndex;
 public class OuttakeAmp extends SequentialCommandGroup {
   /**
    * Scores in the amp
-   * @param arm The arm
-   * @param index The indexer
+   * 
+   * @param arm     The arm
+   * @param index   The indexer
    * @param shooter The shooter
    */
   public OuttakeAmp(Arm arm, StorageIndex index, Shooter shooter) {
     addCommands(
-      // Get the shooter to the right speed while moving arm
-      new InstantCommand(()->shooter.setTargetRPM(ShooterConstants.AMP_OUTTAKE_RPM)),
-      // Move arm
-      new ArmToPos(arm, ArmConstants.ampSetpoint),
-      // Score in amp
-      new InstantCommand(()->index.ejectAmpFront()),
-      // Wait until note is scored
-      new WaitCommand(StorageIndexConstants.ejectAmpFrontTimeout),
-      // Set everything back to default state
-      new InstantCommand(()->shooter.setTargetRPM(0)),
-      new InstantCommand(()->index.stopIndex()),
-      new InstantCommand(()->arm.setAngle(ArmConstants.stowedSetpoint))
-    );
+        // Get the shooter to the right speed while moving arm
+        new InstantCommand(() -> shooter.setTargetRPM(ShooterConstants.AMP_OUTTAKE_RPM)),
+        // Move arm
+        new ArmToPos(arm, ArmConstants.ampSetpoint),
+        // Score in amp
+        new InstantCommand(() -> index.ejectAmpFront()),
+        // Wait until note is scored
+        new WaitCommand(StorageIndexConstants.ejectAmpFrontTimeout),
+        // Set everything back to default state
+        new InstantCommand(() -> shooter.setTargetRPM(0)),
+        new InstantCommand(() -> index.stopIndex()),
+        new InstantCommand(() -> arm.setAngle(ArmConstants.stowedSetpoint)));
   }
 
   /**
    * Aligns to the amp and scores
-   * @param arm The arm
-   * @param index The indexer
+   * 
+   * @param arm     The arm
+   * @param index   The indexer
    * @param shooter The shooter
-   * @param drive The drivetrain
+   * @param drive   The drivetrain
    */
-  public OuttakeAmp(Arm arm, StorageIndex index, Shooter shooter, Drivetrain drive){
-    Pose2d ampPose = DriverStation.getAlliance().get()==Alliance.Red?VisionConstants.RED_AMP_POSE:VisionConstants.BLUE_AMP_POSE;
+  public OuttakeAmp(Arm arm, StorageIndex index, Shooter shooter, Drivetrain drive) {
+    Pose2d ampPose = DriverStation.getAlliance().get() == Alliance.Red ? VisionConstants.RED_AMP_POSE
+        : VisionConstants.BLUE_AMP_POSE;
     addCommands(
-      new SequentialCommandGroup(
-        // Wait until the robot is close enough to the amp to start scoring
-        new WaitUntilCommand((()->{
-          Transform2d dist = drive.getPose().minus(ampPose);
-          return dist.getTranslation().getNorm() < VisionConstants.AMP_TOLERANCE_DISTANCE && Math.abs(dist.getRotation().getRadians()) < VisionConstants.AMP_TOLERANCE_ANGLE;
-        })),
-        // Run the other version of this command to score in the amp
-        new OuttakeAmp(arm, index, shooter)
-      ).deadlineWith(
-        // Go to the pose and stay at it until the command finishes
-        new GoToPose(ampPose,drive),
-      )
-    );
+        new SequentialCommandGroup(
+            // Wait until the robot is close enough to the amp to start scoring
+            new WaitUntilCommand((() -> {
+              Transform2d dist = drive.getPose().minus(ampPose);
+              return dist.getTranslation().getNorm() < VisionConstants.AMP_TOLERANCE_DISTANCE
+                  && Math.abs(dist.getRotation().getRadians()) < VisionConstants.AMP_TOLERANCE_ANGLE;
+            })),
+            // Run the other version of this command to score in the amp
+            new OuttakeAmp(arm, index, shooter)).deadlineWith(
+                // Go to the pose and stay at it until the command finishes
+                new GoToPose(ampPose, drive)));
   }
 }
