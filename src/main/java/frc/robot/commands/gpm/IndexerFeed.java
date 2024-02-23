7f86bd167ba0719782d4eadc17017ed284516746
Arnav495 <arnieincyberland@gmail.com>
2024-02-23 15:20:27 -0800

    Make IndexerFeed extend Command

--- src/main/java/frc/robot/commands/gpm/IndexerFeed.java
+++ src/main/java/frc/robot/commands/gpm/IndexerFeed.java
@@ -1,8 +1,9 @@
 package frc.robot.commands.gpm;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.gpm.StorageIndex;
 
-public class IndexerFeed {
+public class IndexerFeed extends Command {
 
     private final StorageIndex m_index;
 
@@ -10,22 +11,26 @@ public class IndexerFeed {
         this.m_index = index;
     }
 
+	@Override
     public void initialize (){
 
         m_index.ejectIntoShooter();
 
     }
 
+	@Override
     public void execute(){
 
     }
 
+	@Override
     public boolean isFinished(){
 
         return !m_index.hasNote();
         
     }
 
+	@Override
     public void end(boolean interupted){
 
         m_index.stopIndex();
