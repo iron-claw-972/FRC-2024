e989bcc6a1701f4545d1a522b10c4785275cad35
chem7397 <113394068+chem7397@users.noreply.github.com>
2024-02-26 16:17:24 -0800

    add commands

--- src/main/java/frc/robot/controls/Operator.java
+++ src/main/java/frc/robot/controls/Operator.java
@@ -4,8 +4,15 @@
 
 package frc.robot.controls;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.Shoot;
+import frc.robot.commands.gpm.IntakeNote;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.gpm.Arm;
@@ -14,6 +21,7 @@ import frc.robot.subsystems.gpm.Shooter;
 import frc.robot.subsystems.gpm.Intake.Mode;
 import frc.robot.subsystems.gpm.StorageIndex;
 import lib.controllers.GameController;
+import lib.controllers.GameController.Axis;
 import lib.controllers.GameController.Button;
 
 /** Add your docs here. */
@@ -26,7 +34,7 @@ public class Operator {
     private StorageIndex index;
     private Shooter shooter;
     private Drivetrain drive;
-
+    
     public Operator(Intake intake, Arm arm, StorageIndex index, Shooter shooter, Drivetrain drive) {
         this.intake = intake;
         this.arm = arm;
@@ -37,15 +45,21 @@ public class Operator {
 
     public void configureControls() {
         if (intake != null) {
-            kDriver.get(Button.X).onTrue(new InstantCommand(() -> intake.setMode(Mode.INTAKE)));
-            kDriver.get(Button.X).onFalse(new InstantCommand(() -> intake.setMode(Mode.DISABLED)));
+            Command intakeNote = new IntakeNote(intake, index, arm);
+            kDriver.get(Button.X).onTrue(intakeNote);
+            kDriver.get(Button.X).onFalse(new InstantCommand(()->intakeNote.cancel()));
             kDriver.get(Button.B).onTrue(new InstantCommand(() -> intake.setMode(Mode.ReverseMotors)));
             kDriver.get(Button.B).onFalse(new InstantCommand(() -> intake.setMode(Mode.DISABLED)));
         }
-
+        kDriver.get(Button.BACK).onTrue(new InstantCommand(()->CommandScheduler.getInstance().cancelAll()));
         if (index != null && arm != null && shooter != null && drive != null) {
-            kDriver.get(Button.A).whileTrue(new Shoot(shooter, arm, drive, index));
+            getRightTrigger().whileTrue(new Shoot(shooter, arm, drive, index));
         }
-
+    }
+    public Trigger getRightTrigger(){
+        return new Trigger(kDriver.RIGHT_TRIGGER_BUTTON);
+    }
+        public Trigger getLeftTrigger(){
+        return new Trigger(kDriver.LEFT_TRIGGER_BUTTON);
     }
 }
