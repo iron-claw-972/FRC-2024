46f82375d3c5e5727e5216020ba415aaa123e4c1
Pennedrose <tamg6397@lgsstudent.org>
2024-03-02 12:45:51 -0800

    changed arm pid to 0.4

--- src/main/java/frc/robot/subsystems/gpm/Arm.java
+++ src/main/java/frc/robot/subsystems/gpm/Arm.java
@@ -78,7 +78,7 @@ public class Arm extends SubsystemBase {
     // Motor PID control
     private static final double TOLERANCE = Units.degreesToRadians(3.0);
     // P = 5 worked during simulation simulation
-    private static final double P = 0.25;
+    private static final double P = 0.4;
     private static final double I = 0;
     private static final double D = 0;
     private final PIDController pid = new PIDController(P, I, D);
