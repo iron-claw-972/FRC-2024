962c9451f9c01bf215dc736bebd170d50d93ecb6
Arnav495 <arnieincyberland@gmail.com>
2024-03-09 16:37:29 -0800

    Removed redundant code in OuttakeAmp

--- src/main/java/frc/robot/commands/OuttakeAmp.java
+++ src/main/java/frc/robot/commands/OuttakeAmp.java
@@ -59,10 +59,6 @@ public class OuttakeAmp extends SequentialCommandGroup {
    * @param drive   The drivetrain
    */
   public OuttakeAmp(Arm arm, StorageIndex index, Shooter shooter, Drivetrain drive) {
-    Pose2d ampPose = Robot.getAlliance() == Alliance.Red ? VisionConstants.RED_AMP_POSE
-        : VisionConstants.BLUE_AMP_POSE;
-    Pose2d ampPose2 = Robot.getAlliance() == Alliance.Red ? VisionConstants.RED_AMP_POSE //TODO
-        : VisionConstants.BLUE_AMP_POSE; //TODO FIX this
     addCommands(
       new InstantCommand(()->getPoses()),
       new SequentialCommandGroup(
