34e22d3db78bdf2c1a881123ad8add80b0d73aae
Saara21 <pips7836@lgsstudent.org>
2024-03-20 14:57:41 -0700

    added all paths to hyperconverged

--- src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
+++ src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
@@ -28,13 +28,33 @@ public class AutoTab extends ShuffleBoardTabs {
     
         // Final Autos
         autoCommand.setDefaultOption("Do nothing", new DoNothing());
-        autoCommand.addOption("God Path", new FollowPathCommand("God Path",true, drive));
-        autoCommand.addOption("Test", new FollowPathCommand("Test",true, drive));
+
+        // 1 piece
+        autoCommand.addOption("1 piece pos4", new FollowPathCommand("1 piece pos4",true, drive));
+        autoCommand.addOption("1.5 piece (pos 4) (B)", new FollowPathCommand("1.5 piece (pos 4) (B)",true, drive));
+        autoCommand.addOption("pos 3 stage", new FollowPathCommand("pos 3 stage",true, drive));
+         autoCommand.addOption("Stage 1 piece pos4", new FollowPathCommand("Stage 1 piece pos4",true, drive));
+       // 2 piece
+        autoCommand.addOption("2.5 Piece (pos 2) (B)", new FollowPathCommand("2.5 Piece (pos 2) (B)",true, drive));
+        autoCommand.addOption("Two Piece (pos 4) center line", new FollowPathCommand("Two Piece (pos 4) center line",true, drive));
+        // 3 piece
+        autoCommand.addOption("Three Piece (pos 2) (B)", new FollowPathCommand("Three Piece (pos 2) (B)",true, drive));
+        autoCommand.addOption("Three Piece (pos 4) understage", new FollowPathCommand("Three Piece (pos 4) understage",true, drive));
+        autoCommand.addOption("[2] Three Piece (pos 2) [B]", new FollowPathCommand("[2] Three Piece (pos 2) [B]",true, drive));
+       // 4 piece
+       autoCommand.addOption("4 piece (pos 3)", new FollowPathCommand("4 piece (pos 3)",true, drive));
+        autoCommand.addOption("4.5 piece (pos 3) (normal)", new FollowPathCommand("4.5 piece (pos 3) (normal)",true, drive));
+        // 5 piece
+       
         autoCommand.addOption("5 piece (pos 3) (normal)", new FollowPathCommand("5 piece (pos 3) (normal)",true, drive));
         autoCommand.addOption("5 piece (pos 3) (test)", new FollowPathCommand("5 piece (pos 3) (test)",true, drive));
         autoCommand.addOption("5 piece (pos 3) (under the stage -- test)", new FollowPathCommand("5 piece (pos 3) (under the stage -- test)",true, drive));
-        autoCommand.addOption("Test1", new FollowPathCommand("Test1",true, drive));
-
+       
+        
+        // extra
+         autoCommand.addOption("God Path", new FollowPathCommand("God Path",true, drive));
+        autoCommand.addOption("Test", new FollowPathCommand("Test",true, drive));
+         autoCommand.addOption("Test1", new FollowPathCommand("Test1",true, drive));
         
         // Previous Autos (Some will keep and still have to fix) autoCommand.setDefaultOption("Do Nothing", new PrintCommand("This will do nothing!"));
         // autoCommand.addOption("Example Path", new FollowPathCommand("Example Path",true, drive));
