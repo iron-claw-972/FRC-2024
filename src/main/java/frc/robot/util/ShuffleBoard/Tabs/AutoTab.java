c480ca25155edd03628733b956b57323e53b6a88
Tyrus <businessomga@gmail.com>
2024-03-26 15:58:22 -0700

    made paths

--- src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
+++ src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
@@ -7,9 +7,8 @@ package frc.robot.util.ShuffleBoard.Tabs;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.DoNothing;
-import frc.robot.commands.auto_comm.AutoShootCommand;
-import frc.robot.commands.auto_comm.ChoreoPathCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.auto_comm.FollowPathCommand;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.gpm.Shooter;
@@ -30,31 +29,67 @@ public class AutoTab extends ShuffleBoardTabs {
     
     public void createEntries(){  
         tab = Shuffleboard.getTab("Auto");
-    
+
+        autoCommand.setDefaultOption("Choreo Distance Center 6 (no shoot)", new SequentialCommandGroup(
+                new FollowPathCommand("Distance Center 6.1", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 6.2", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 6.3", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 6.4", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 6.5", true, drive)
+        ));
+
+        autoCommand.setDefaultOption("Choreo Distance Center 7 (no shoot)", new SequentialCommandGroup(
+                new FollowPathCommand("Distance Center 7.1", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 7.2", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 7.3", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 7.4", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 7.5", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 7.6", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Center 7.7", true, drive)
+        ));
+
+        autoCommand.setDefaultOption("Choreo Distance Source 4 (no shoot)", new SequentialCommandGroup(
+                new FollowPathCommand("Distance Source 6.1", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Source 6.2", true, drive),
+                new WaitCommand(1),
+                new FollowPathCommand("Distance Source 6.3", true, drive)
+        ));
+
         // Final Autos
-        autoCommand.setDefaultOption("Do nothing", new DoNothing());
-        autoCommand.addOption("God Path", new FollowPathCommand("God Path",true, drive));
-        autoCommand.addOption("Test", new FollowPathCommand("Test",true, drive));
-        autoCommand.addOption("5 piece (pos 3) (normal)", new FollowPathCommand("5 piece (pos 3) (normal)",true, drive));
-        autoCommand.addOption("5 piece (pos 3) (test)", new FollowPathCommand("5 piece (pos 3) (test)",true, drive));
-        autoCommand.addOption("5 piece (pos 3) (under the stage -- test)", new FollowPathCommand("5 piece (pos 3) (under the stage -- test)",true, drive));
-        autoCommand.addOption("Test1", new FollowPathCommand("Test1",true, drive));
-        autoCommand.addOption("Sabotage Trial (1)", new FollowPathCommand("Sabotage Trial (1)",true, drive));
-        autoCommand.addOption("5 piece (pos 3) (under the stage -- test)", new FollowPathCommand("5 piece (pos 3) (under the stage -- test)",true, drive));
-        autoCommand.addOption("Far", new FollowPathCommand("Far",true, drive));
-        autoCommand.addOption("3 Source (pos 4) (B)", new FollowPathCommand("3 Source (pos 4) (B)",true, drive));
-        autoCommand.addOption("Three Source (pos 4) center line", new FollowPathCommand("Three Piece (pos 4) center line",true, drive));
-        autoCommand.addOption("2.5 Piece (pos 2) (B)", new FollowPathCommand("2.5 Piece (pos 2) (B)",true, drive));
-        autoCommand.addOption("Choreo Center 6", new AutoShootCommand(
-                shooter,
-                1750,
-                new ChoreoPathCommand("Center 6", true, drive)
-        ));
-        autoCommand.addOption("Choreo Source 3", new AutoShootCommand(
-                shooter,
-                1750,
-                new ChoreoPathCommand("Source 3", true, drive)
-        ));
+//        autoCommand.setDefaultOption("Do nothing", new DoNothing());
+//        autoCommand.addOption("God Path", new FollowPathCommand("God Path",true, drive));
+//        autoCommand.addOption("Test", new FollowPathCommand("Test",true, drive));
+//        autoCommand.addOption("5 piece (pos 3) (normal)", new FollowPathCommand("5 piece (pos 3) (normal)",true, drive));
+//        autoCommand.addOption("5 piece (pos 3) (test)", new FollowPathCommand("5 piece (pos 3) (test)",true, drive));
+//        autoCommand.addOption("5 piece (pos 3) (under the stage -- test)", new FollowPathCommand("5 piece (pos 3) (under the stage -- test)",true, drive));
+//        autoCommand.addOption("Test1", new FollowPathCommand("Test1",true, drive));
+//        autoCommand.addOption("Sabotage Trial (1)", new FollowPathCommand("Sabotage Trial (1)",true, drive));
+//        autoCommand.addOption("5 piece (pos 3) (under the stage -- test)", new FollowPathCommand("5 piece (pos 3) (under the stage -- test)",true, drive));
+//        autoCommand.addOption("Far", new FollowPathCommand("Far",true, drive));
+//        autoCommand.addOption("3 Source (pos 4) (B)", new FollowPathCommand("3 Source (pos 4) (B)",true, drive));
+//        autoCommand.addOption("Three Source (pos 4) center line", new FollowPathCommand("Three Piece (pos 4) center line",true, drive));
+//        autoCommand.addOption("2.5 Piece (pos 2) (B)", new FollowPathCommand("2.5 Piece (pos 2) (B)",true, drive));
+//        autoCommand.addOption("Choreo Center 6", new AutoShootCommand(
+//                shooter,
+//                1750,
+//                new ChoreoPathCommand("Center 6", true, drive)
+//        ));
+//        autoCommand.addOption("Choreo Source 3", new AutoShootCommand(
+//                shooter,
+//                1750,
+//                new ChoreoPathCommand("Source 3", true, drive)
+//        ));
 
         // Previous Autos (Some will keep and still have to fix) autoCommand.setDefaultOption("Do Nothing", new PrintCommand("This will do nothing!"));
         // autoCommand.addOption("Example Path", new FollowPathCommand("Example Path",true, drive));
