bd9d907efd687548d52cac09f3b648f2577aed19
omegaladon <businessomga@gmail.com>
2024-03-30 14:10:23 -0700

    Revert "Merge branch 'main' into Choreo-After-CVR"
    This reverts commit a805cfc8e1a645636778b5cc6f5ee7206631a650, reversing
    changes made to e495df3033a1c62f138f53d8e680f2d45fb921a9.

--- src/main/java/frc/robot/controls/Operator.java
+++ src/main/java/frc/robot/controls/Operator.java
@@ -4,23 +4,27 @@
 
 package frc.robot.controls;
 
+import java.util.function.Consumer;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Robot;
+import frc.robot.commands.GoToPose;
 import frc.robot.commands.IntakeWithRumble;
 import frc.robot.commands.OuttakeAmp;
-import frc.robot.commands.OuttakeAmpManual;
 import frc.robot.commands.Shoot;
 import frc.robot.commands.ShootLock;
+import frc.robot.commands.gpm.IntakeNote;
 import frc.robot.commands.gpm.PrepareShooter;
 import frc.robot.commands.gpm.ShootKnownPos;
 import frc.robot.commands.gpm.ShootKnownPos.ShotPosition;
 import frc.robot.constants.ArmConstants;
 import frc.robot.constants.Constants;
 import frc.robot.constants.ShooterConstants;
+import frc.robot.constants.miscConstants.VisionConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.gpm.Arm;
 import frc.robot.subsystems.gpm.Intake;
@@ -29,6 +33,7 @@ import frc.robot.subsystems.gpm.Intake.Mode;
 import frc.robot.subsystems.gpm.StorageIndex;
 import lib.controllers.GameController;
 import lib.controllers.GameController.Button;
+import lib.controllers.GameController.DPad;
 import lib.controllers.GameController.RumbleStatus;
 
 /** Add your docs here. */
@@ -41,32 +46,33 @@ public class Operator {
     private StorageIndex index;
     private Shooter shooter;
     private Drivetrain drive;
-
-    private boolean ampOrder = false; 
-
-    public Operator(Intake intake, Arm arm, StorageIndex index, Shooter shooter, Drivetrain drive) {
+    private Consumer<Boolean> consumer;
+    IntakeNote intakeNote;
+    
+    public Operator(Intake intake, Arm arm, StorageIndex index, Shooter shooter, Drivetrain drive, Consumer<Boolean> consumer) {
         this.intake = intake;
         this.arm = arm;
         this.index = index;
         this.shooter = shooter;
         this.drive = drive;
+        this.consumer = consumer;
     }
 
     public void configureControls() {
-        // if (intake != null) {
-        //     Command inakeWithRumble =  new IntakeWithRumble(intake, index, arm, (x)->{
-        //         if (x){
-        //             kDriver.setRumble(RumbleStatus.RUMBLE_ON);
-        //         }
-        //         else{
-        //             kDriver.setRumble(RumbleStatus.RUMBLE_OFF);
-        //         }
-        //     });
-        //     kDriver.get(Button.X).onTrue(inakeWithRumble);
-        //     kDriver.get(Button.X).onFalse(new InstantCommand(()->inakeWithRumble.cancel()));
+        if (intake != null) {
+           // Command intakeNote = new IntakeNote(intake, index, arm);
+          // kDriver.setRumble(RumbleStatus.RUMBLE_ON);
+
+            kDriver.get(Button.B).onTrue(new InstantCommand(() -> intake.setMode(Mode.ReverseMotors),intake));
+            kDriver.get(Button.B).onFalse(new InstantCommand(() -> intake.setMode(Mode.DISABLED), intake));
+        }
+        if (intake != null) {
+            intakeNote = new IntakeNote(intake, index, arm, consumer);
+            kDriver.get(Button.X).onTrue(intakeNote);
+            kDriver.get(Button.X).onFalse(new InstantCommand(()->intakeNote.cancel()));
         //     kDriver.get(Button.B).onTrue(new InstantCommand(() -> intake.setMode(Mode.ReverseMotors),intake));
         //     kDriver.get(Button.B).onFalse(new InstantCommand(() -> intake.setMode(Mode.DISABLED), intake));
-        // }
+        }
         if(index != null){
             kDriver.get(Button.B).onTrue(new InstantCommand(() -> index.ejectBack(), index));
             kDriver.get(Button.B).onFalse(new InstantCommand(() -> index.stopIndex(), index));
@@ -74,6 +80,7 @@ public class Operator {
         kDriver.get(Button.BACK).onTrue(new InstantCommand(()->{
             if(shooter != null){
                 shooter.setTargetRPM(0);
+                shooter.resetPID();
             }
             if(intake != null){
                 intake.setMode(Mode.DISABLED);
@@ -90,41 +97,26 @@ public class Operator {
             getRightTrigger().onTrue(new Shoot(shooter, arm, drive, index));
         }
         if(shooter != null){
-            getLeftTrigger().onTrue(new PrepareShooter(shooter, Shooter.addSlip(Shooter.shooterSpeedToRPM(ShooterConstants.SHOOT_SPEED_MPS))));
+            getLeftTrigger().onTrue(new PrepareShooter(shooter, Shooter.addSlip(Shooter.shooterSpeedToRPM(ShooterConstants.SHOOT_SPEED_MPS-1))));
         }
         if(arm != null && shooter != null && index != null){
             kDriver.get(Button.Y).onTrue(new ShootKnownPos(shooter, arm, index, ShotPosition.SUBWOOFER));
-            // First button press sets arm and spins shooter and second button press outtakes note
-            // and returns to default state
-            kDriver.get(Button.A).onTrue(new SequentialCommandGroup(
-                                            new InstantCommand(() -> {ampOrder=!ampOrder;}),
-                                            new ConditionalCommand(
-                                                new OuttakeAmpManual(arm, shooter), 
-                                                new OuttakeAmpManual(arm, shooter, index), 
-                                                () -> ampOrder)));
+            kDriver.get(Button.A).onTrue(new OuttakeAmp(arm, index, shooter));
         }
         if(arm != null){
             kDriver.get(Button.RB).onTrue(new InstantCommand(()->arm.setAngle(ArmConstants.preClimbSetpoint), arm));
             kDriver.get(Button.LB).onTrue(new InstantCommand(()->arm.setAngle(ArmConstants.climbSetpoint), arm));
           }
-        if (intake!=null && arm!=null&& index!=null){
-            Command inakeWithRumble =  new IntakeWithRumble(intake, index, arm, (x)->{
-                if (x){
-                    kDriver.setRumble(RumbleStatus.RUMBLE_ON);
-                }
-                else{
-                    kDriver.setRumble(RumbleStatus.RUMBLE_OFF);
-                }
-            });
-            kDriver.get(Button.X).onTrue(inakeWithRumble);
-            //kDriver.get(Button.X).toggleOnTrue(intakeNote);
-            kDriver.get(Button.X).onFalse(new InstantCommand(()->inakeWithRumble.cancel()));
-        }
+           // Align to subwoofer
+   
     }
     public Trigger getRightTrigger(){
         return new Trigger(kDriver.RIGHT_TRIGGER_BUTTON);
     }
     public Trigger getLeftTrigger(){
         return new Trigger(kDriver.LEFT_TRIGGER_BUTTON);
+    }
+    public GameController getGameController(){
+        return kDriver;
     }
 }
